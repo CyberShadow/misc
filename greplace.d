572374a1a67ab919dedbd926f237cd44b542578e
Vladimir Panteleev <git@thecybershadow.net>
2016-09-07 03:52:01 +0000

    greplace: Add symlink support

--- greplace.d
+++ greplace.d
@@ -10,8 +10,9 @@ import std.string;
 
 void main(string[] args)
 {
-	bool force;
+	bool force, wide;
 	getopt(args,
+		"w", &wide,
 		"f", &force);
 
 	if (args.length != 3)
@@ -23,57 +24,80 @@ void main(string[] args)
 	if (!force)
 	{
 		foreach (file; files)
-			if (isFile(file))
-			{
-				auto data = cast(ubyte[])std.file.read(file);
-				if (data.countUntil(to)>=0)
-					throw new Exception("File " ~ file ~ " already contains " ~ args[2]);
-				if (data.countUntil(tow)>=0)
-					throw new Exception("File " ~ file ~ " already contains " ~ args[2] ~ " (in UTF-16)");
-			}
+		{
+			ubyte[] data;
+			if (file.isSymlink())
+				data = cast(ubyte[])readLink(file);
+			else
+			if (file.isFile())
+				data = cast(ubyte[])std.file.read(file);
+			else
+				continue;
+
+			if (data.countUntil(to)>=0)
+				throw new Exception("File " ~ file ~ " already contains " ~ args[2]);
+			if (data.countUntil(tow)>=0)
+				throw new Exception("File " ~ file ~ " already contains " ~ args[2] ~ " (in UTF-16)");
+		}
 	}
 
 	foreach (file; files)
 	{
-		if (isFile(file))
+		ubyte[] s;
+		if (file.isSymlink())
+			s = cast(ubyte[])readLink(file);
+		else
+		if (file.isFile())
+			s = cast(ubyte[])std.file.read(file);
+		else
+			continue;
+
+		bool modified = false;
+		if (s.countUntil(from)>=0)
 		{
-			auto s = cast(ubyte[])std.file.read(file);
-			bool modified = false;
-			if (s.countUntil(from)>=0)
-			{
-				s = s.replace(from, to);
-				modified = true;
-			}
-			if (s.countUntil(fromw)>=0)
-			{
-				s = s.replace(fromw, tow);
-				modified = true;
-			}
-			if (modified)
-			{
-				writeln(file);
+			s = s.replace(from, to);
+			modified = true;
+		}
+		if (wide && s.countUntil(fromw)>=0)
+		{
+			s = s.replace(fromw, tow);
+			modified = true;
+		}
+
+		if (modified)
+		{
+			writeln(file);
+
+			if (file.isFile())
 				std.file.write(file, s);
-			}
-
-			if (file.indexOf(args[1])>=0)
+			else
+			if (file.isSymlink())
 			{
-				string newName = file.replace(args[1], args[2]);
-				writeln(file, " -> ", newName);
-	
-				if (!exists(dirName(newName)))
-					mkdirRecurse(dirName(newName));
-				std.file.rename(file, newName);
-	
-				// TODO: empty folders
-
-				auto segments = array(pathSplitter(file))[0..$-1];
-				foreach_reverse (i; 0..segments.length)
-				{
-					auto dir = buildPath(segments[0..i+1]);
-					if (array(map!`a.name`(dirEntries(dir, SpanMode.shallow))).length==0)
-						rmdir(dir);
-				}	
+				remove(file);
+				symlink(cast(string)s, file);
 			}
-		}	
+			else
+				assert(false);
+		}
+
+		if (file.indexOf(args[1])>=0)
+		{
+			string newName = file.replace(args[1], args[2]);
+			writeln(file, " -> ", newName);
+	
+			if (!exists(dirName(newName)))
+				mkdirRecurse(dirName(newName));
+			std.file.rename(file, newName);
+	
+			// TODO: empty folders
+
+			auto segments = array(pathSplitter(file))[0..$-1];
+			foreach_reverse (i; 0..segments.length)
+			{
+				auto dir = buildPath(segments[0..i+1]);
+				if (array(map!`a.name`(dirEntries(dir, SpanMode.shallow))).length==0)
+					rmdir(dir);
+			}	
+		}
 	}
 }
