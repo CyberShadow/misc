0d394a5281b6dc818530fbdf609d38df795fe6e6
Vladimir Panteleev <git@thecybershadow.net>
2017-04-03 21:05:24 +0000

    Add unpack.d

---
+++ unpack.d
@@ -0,0 +1,16 @@
+import std.array;
+import std.file;
+import std.path;
+
+import ae.sys.archive;
+import ae.utils.funopt;
+import ae.utils.main;
+
+void unpack(string fileName)
+{
+	auto dir = fileName.replace(".tar.", ".").stripExtension;
+	mkdir(dir);
+	ae.sys.archive.unpack(fileName, dir);
+}
+
+mixin main!(funopt!unpack);
