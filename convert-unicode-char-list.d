d8f8fc146f82890832ee1edd34ede7ceb397fda6
Vladimir Panteleev <git@thecybershadow.net>
2017-04-26 08:21:21 +0000

    convert-unicode-char-list: Filter out UTF errors

--- convert-unicode-char-list.d
+++ convert-unicode-char-list.d
@@ -3,12 +3,16 @@ module convert;
 import std.array;
 import std.conv;
 import std.stdio;
+import std.utf;
 
 void main()
 {
 	foreach (line; stdin.byLine)
 	{
 		auto parts = line.split(";");
-		writefln("%c - %s", cast(dchar)(parts[0].to!int(16)), parts[1]);
+		try
+			writefln("%c - %s", cast(dchar)(parts[0].to!int(16)), parts[1]);
+		catch (UTFException)
+			continue;
 	}
 }
