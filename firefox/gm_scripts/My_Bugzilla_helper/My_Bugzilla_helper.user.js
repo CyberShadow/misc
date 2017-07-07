f1648ebda2d22c20f6eacecd307d26f0e9ede462
Vladimir Panteleev <git@thecybershadow.net>
2017-07-07 06:41:24 +0000

    firefox: Add My_Bugzilla_helper

---
+++ firefox/gm_scripts/My_Bugzilla_helper/My_Bugzilla_helper.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        My Bugzilla helper
+// @namespace   thecybershadow.net
+// @include     https://issues.dlang.org/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+  var eSummary = document.getElementById('summary_alias_container');
+  var eButton = document.createElement('a');
+  var eId = document.getElementsByName('id')[0];
+  eButton.href = 'emacs:///home/vladimir/work/extern/D/DBugTests/bugs/' + eId.value;
+  eButton.innerHTML = 'Open in Emacs';
+  eSummary.appendChild(eButton);
+})();
