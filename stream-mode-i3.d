c124880c83273a8f7a7e049d4d919267d253a1f3
Vladimir Panteleev <git@thecybershadow.net>
2017-07-16 02:15:59 +0000

    stream-mode-i3: Use i3-msg

--- stream-mode-i3.d
+++ stream-mode-i3.d
@@ -10,8 +10,8 @@ import std.stdio;
 
 void main()
 {
-	auto treeRes = execute(["i3-save-tree", "--OUTPUT=DP-1.8"]);
-	enforce(treeRes.status == 0, "i3-save-tree failed");
+	auto treeRes = execute(["i3-msg", "-t", "get_tree"]);
+	enforce(treeRes.status == 0, "i3-msg failed");
 	auto treeStr = treeRes.output;
 
 	treeStr = treeStr
@@ -20,37 +20,68 @@ void main()
 		.replaceAll(re!"\n\n\\{", ",\n{")
 	;
 
-	static struct Geometry
+	static struct Rect
 	{
 		int height, width, x, y;
 	}
 
 	static struct Spec
 	{
-		@JSONName("class")
-		string class_;
-		string instance, title, transient_for, window_role;
 	}
 
 	static struct Node
 	{
-		string border;
-		string current_border_width;
-		string floating;
-		int fullscreen_mode;
-		string layout;
-		Geometry geometry;
-		string name;
-		real percent;
+		int id;
 		string type;
+		string orientation;
+		string scratchpad_state;
+		real* percent;
+		bool urgent;
+		bool focused;
+		string output;
+		string layout;
+		string workspace_layout;
+		string last_split_layout;
+		string border;
+		int current_border_width;
+		Rect rect;
+		Rect deco_rect;
+		Rect window_rect;
+		Rect geometry;
+		string name;
+		int num;
+		struct Gaps { int inner; int outer; }
+		Gaps gaps;
+		int* window;
+		struct Props { @JSONName("class") string class_; string instance, title, transient_for, window_role; }
+		Props window_properties;
 		Node[] nodes;
-		Spec[] swallows;
+		Node[] floating_nodes;
+		int[] focus;
+		int fullscreen_mode;
+		bool sticky;
+		string floating;
+		struct Swallow { int dock, insert_where; }
+		Swallow[] swallows; // ?
 	}
 
-	treeStr = "[" ~ treeStr ~ "]";
+	//treeStr = "[" ~ treeStr ~ "]";
 
-	auto workspaces = jsonParse!(Node[])(treeStr);
-	writeln(treeStr);
+	auto root = jsonParse!Node(treeStr);
+	enforce(root.type == "root");
+	foreach (output; root.nodes)
+	{
+		enforce(output.type == "output");
+		if (output.name != "DP-1.8")
+			continue;
 
-	
+		foreach (onode; output.nodes)
+			if (onode.type == "con")
+				foreach (ws; onode.nodes)
+				{
+					enforce(ws.type == "workspace");
+					if (ws.layout != "splitv")
+					writeln(ws.type, " ", ws.name, " ", ws.layout);
+				}
+	}
 }
