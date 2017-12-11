c389a8e1e7a39bfe634b8a750f463346a47798d8
Vladimir Panteleev <git@thecybershadow.net>
2017-12-11 05:42:36 +0000

    applayout: Read rules from file

--- applayout.d
+++ applayout.d
@@ -1,15 +1,32 @@
 /// Automatically switch keyboard layouts depending on the title of the focused window.
 /// Mainly intended for games with non-configurable key bindings.
-/// Currently the configuration is hard-coded.
+/// Configuration file: ~/.config/applayout.txt
+/// Configuration format: <layout number> <TAB> <window title>
 module applayout;
 
+import std.algorithm.iteration;
+import std.algorithm.searching;
+import std.array;
 import std.conv;
+import std.file;
+import std.path;
 import std.process;
 import std.stdio;
 import std.string;
+import std.typecons;
+
+import ae.sys.persistence.core;
 
 void main()
 {
+	auto rules = FileCache!((string fn) => fn
+		.readText
+		.splitLines
+		.filter!(line => line.length && !line.startsWith("#") && line.canFind("\t"))
+		.map!((line) { auto parts = line.findSplit("\t"); return tuple(parts[2], parts[0].to!int); })
+		.assocArray
+	)("~/.config/applayout.txt".expandTilde);
+
 	auto p = pipe();
 	auto pid = spawnProcess(["xtitle", "-s"], stdin, p.writeEnd);
 	auto f = p.readEnd;
@@ -17,17 +34,7 @@ void main()
 	while (!f.eof)
 	{
 		auto title = f.readln().chomp();
-		int layout;
-		switch (title)
-		{
-			case "Uurnog":
-			case "Tallowmere":
-			case "UNDERTALE":
-			case "Opus Magnum":
-				layout = 2; break;
-			default:
-				layout = 1; break;
-		}
+		int layout = rules.get(title, 1);
 		if (layout != oldLayout)
 		{
 			stderr.writefln("Switching layout from %d to %d for %s", oldLayout, layout, title);
