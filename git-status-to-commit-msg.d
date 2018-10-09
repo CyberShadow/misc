51310916b196c87695fb167a592ab341c1f5c48e
Vladimir Panteleev <git@thecybershadow.net>
2018-10-09 23:29:00 +0000

    git-*-commit-msg: Support non-D stuff

--- git-status-to-commit-msg.d
+++ git-status-to-commit-msg.d
@@ -3,7 +3,7 @@
    modified. To be used with the git-prepare-commit-msg hook.
 */
 
-module git_prepare_commit_msg;
+module git_status_to_commit_msg;
 
 import std.algorithm;
 import std.array;
@@ -19,23 +19,44 @@ void main(string[] args)
 {
 	string prefix = args.length > 1
 		? args[1]
-		: getcwd.buildNormalizedPath(environment.get("GIT_DIR", ".git")).dirName().baseName() ~ ".";
+		: getcwd.buildNormalizedPath(environment.get("GIT_DIR", ".git")).dirName().baseName();
+
+	string[] lines;
+	while (!stdin.eof)
+		lines ~= readln().chomp().idup;
+
+	auto files = lines.filter!(line => line.length && line[0].isOneOf("AM")).map!(line => line[3..$]).array;
+	string targetExt;
+	foreach (ext; [".d", ".el", ".bash", ".sh"])
+		if (files.any!(file => file.endsWith(ext)))
+		{
+			targetExt = ext;
+			break;
+		}
+	auto extensions = files.map!extension.array.sort.uniq.array;
+	if (extensions.length == 1)
+		targetExt = extensions[0];
 
 	string packStaged, packWD;
-	while (!stdin.eof)
+	foreach (line; lines)
 	{
-		auto line = readln().chomp();
 		if (line.length > 3)
 		{
 			void handleLine(string line, ref string pack)
 			{
-				if (!line.endsWith(".d"))
+				if (targetExt.length && !line.endsWith(targetExt))
 					return;
-				auto mod = line[0..$-2].replace("/", ".");
-				if (!mod.skipOver("src."))
-					mod = prefix ~ mod;
+				auto mod = line[0 .. $ - targetExt.length];
+				char delim = '/';
+				if (targetExt == ".d")
+				{
+					delim = '.';
+					mod = mod.replace("/", [delim]);
+					if (!mod.skipOver("src" ~ delim))
+						mod = prefix ~ delim ~ mod;
+				}
 				if (pack)
-					pack = commonPrefix(mod, pack).stripRight('.');
+					pack = commonPrefix(mod, pack).stripRight(delim);
 				else
 					pack = mod;
 			}
