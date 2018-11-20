f8c1faefd8c77f5ad85fb9cd84dafe54911af4c2
Vladimir Panteleev <git@thecybershadow.net>
2018-11-20 09:17:53 +0000

    dtestre: Initial commit

---
+++ dtestre.d
@@ -0,0 +1,35 @@
+/**
+   Parse some D regular expressions from the command line.
+   For each line read from stdin, display the match result against those expressions.
+*/
+
+import std.algorithm.iteration;
+import std.array;
+import std.exception;
+import std.regex;
+import std.stdio;
+import std.string;
+
+import ae.utils.funopt;
+import ae.utils.main;
+
+void dTestRE(string[] regexps)
+{
+	auto res = regexps.map!regex.array;
+	while (!stdin.eof)
+	{
+		auto line = stdin.readln().chomp();
+		foreach (i, re; res)
+		{
+			if (i) write("\t");
+			auto m = matchFirst(line, re);
+			if (m)
+				write(m.array);
+			else
+				write("(no match)");
+		}
+		writeln;
+	}
+}
+
+mixin main!(funopt!dTestRE);
