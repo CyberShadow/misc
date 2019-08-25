03d5cf98128a12a08e1be9afd8ea1507270a59ca
Vladimir Panteleev <git@thecybershadow.net>
2019-08-25 01:03:27 +0000

    urlencode: Add --delim

--- urlencode.d
+++ urlencode.d
@@ -2,36 +2,52 @@
 
 import std.stdio;
 import std.ascii;
+import std.typecons;
 
 import ae.utils.funopt;
 import ae.utils.main;
 import ae.utils.text;
 
-void urlEncodeFile(ref File f, ref File o, bool all)
+void urlEncodeFile(ref File f, ref File o, bool all, string delim)
 {
+	void doChar(char c)
+	{
+		if (!all && isAlphaNum(c))
+			o.rawWrite((&c)[0..1]);
+		else
+		{
+			char[3] x;
+			x[0] = '%';
+			static const HEX = "0123456789ABCDEF";
+			x[1] = HEX[cast(ubyte)c >> 4];
+			x[2] = HEX[cast(ubyte)c & 15];
+			o.rawWrite(x[]);
+		}
+	}
+
 	while (!f.eof)
 	{
-		char c;
-		if (f.rawRead((&c)[0..1]))
+		if (delim)
 		{
-			if (!all && isAlphaNum(c))
-				o.rawWrite((&c)[0..1]);
-			else
+			foreach (line; f.byLine(No.keepTerminator, delim))
 			{
-				char[3] x;
-				x[0] = '%';
-				static const HEX = "0123456789ABCDEF";
-				x[1] = HEX[cast(ubyte)c >> 4];
-				x[2] = HEX[cast(ubyte)c & 15];
-				o.rawWrite(x[]);
+				foreach (char c; line)
+					doChar(c);
+				o.rawWrite(delim);
 			}
+		}
+		else
+		{
+			char c;
+			if (f.rawRead((&c)[0..1]))
+				doChar(c);
 		}
 	}
 }
 
-void urlEncode(bool all)
+void urlEncode(bool all, string delim = null)
 {
-	urlEncodeFile(stdin, stdout, all);
+	urlEncodeFile(stdin, stdout, all, delim);
 }
 
 mixin main!(funopt!urlEncode);
