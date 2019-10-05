3b0f2c1c8bedd585dcd8be777946f9ea0e5d1b56
Vladimir Panteleev <git@thecybershadow.net>
2019-10-05 14:39:10 +0000

    chunkify: Initial commit

---
+++ chunkify.d
@@ -0,0 +1,23 @@
+import std.algorithm.iteration;
+import std.format;
+import std.stdio;
+
+import ae.utils.funopt;
+import ae.utils.main;
+
+import chunker;
+import chunker.polynomials;
+
+void chunkify(string[] files)
+{
+	foreach (fn; files)
+	{
+		size_t chunkIndex = 0;
+		File(fn, "rb")
+			.byChunk(1024*1024)
+			.byCDChunk(Pol(0x3DA3358B4DC173))
+			.each!(chunk => chunk.data.toFile(format("%s.chunk%04d", fn, chunkIndex++)));
+	}
+}
+
+mixin main!(funopt!chunkify);
