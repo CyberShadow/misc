07dd0c464f7f2d82387f931bb75e844212e7cecf
Vladimir Panteleev <git@thecybershadow.net>
2020-05-14 01:19:43 +0000

    git-find-file-with-sha1: Add --first

--- git-find-file-with-sha1.d
+++ git-find-file-with-sha1.d
@@ -14,6 +14,7 @@ import ae.utils.meta;
 import ae.utils.text;
 
 import std.algorithm.iteration;
+import std.algorithm.searching;
 import std.array;
 import std.digest.sha;
 import std.stdio;
@@ -21,6 +22,7 @@ import std.string;
 
 int git_find_file_with_sha1(
 	bool all,
+	bool first,
 	string pathStr,
 	string sha1,
 )
@@ -32,41 +34,71 @@ int git_find_file_with_sha1(
 	// Current search heads for breadth-first search
 	Hash[] commits = [repo.query(`rev-parse`, `HEAD`).toCommitHash()];
 
-	string[Hash] hashCache;
+	bool[Hash] blobCache;
+	bool checkBlobHash(Hash blobHash)
+	{
+		return blobCache.require(blobHash, {
+			auto blob = reader.read(blobHash).data;
+			return blob.getDigestString!SHA1().toLower == sha1;
+		}());
+	}
+
+	struct CommitResult { enum Status { gone, bad, good } Status status; Hash[] parents; }
+	CommitResult[Hash] commitCache;
+	CommitResult checkCommitHash(Hash commitHash)
+	{
+		return commitCache.require(commitHash, {
+			auto commit = reader.read(commitHash).parseCommit();
+
+			CommitResult result;
+			result.parents = commit.parents;
+
+			auto fileHash = commit.tree;
+			foreach (dir; path)
+			{
+				auto match = fileHash
+					.I!(t => reader.read(t))
+					.parseTree()
+					.filter!(e => e.name == dir);
+				if (match.empty)
+					return result;
+				fileHash = match
+					.front
+					.hash;
+			}
+
+			result.status = checkBlobHash(fileHash) ? CommitResult.Status.good : CommitResult.Status.bad;
+			return result;
+		}());
+	}
+
 	HashSet!Hash visited;
 	bool found;
 
-commitLoop:
 	while (commits.length)
 	{
 		auto commitHash = commits.shift();
-			if (commitHash in visited)
-				continue;
-			visited.add(commitHash);
-		auto commit = reader.read(commitHash).parseCommit();
-		auto fileHash = commit.tree;
-		foreach (dir; path)
-		{
-			auto match = fileHash
-				.I!(t => reader.read(t))
-				.parseTree()
-				.filter!(e => e.name == dir);
-			if (match.empty)
-				continue commitLoop;
-			fileHash = match
-				.front
-				.hash;
-		}
+		if (commitHash in visited)
+			continue;
+		visited.add(commitHash);
 
 		// stderr.writeln(fileHash.toString());
-		if (hashCache.require(fileHash, reader.read(fileHash).data.getDigestString!SHA1().toLower) == sha1)
+		auto result = checkCommitHash(commitHash);
+		if (result.status == CommitResult.Status.gone)
+			continue;
+
+		bool ok = result.status == CommitResult.Status.good;
+		if (first)
+			ok = ok && result.parents.all!(parent => checkCommitHash(parent).status != CommitResult.Status.good);
+
+		if (ok)
 		{
 			writeln(commitHash.toString());
 			found = true;
 			if (!all)
 				return 0;
 		}
-		commits ~= commit.parents;
+		commits ~= result.parents;
 	}
 	if (found)
 		return 0;
