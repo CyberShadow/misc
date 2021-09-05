c01edd7e8bda1bf1e7d6ab963dcc401b44e34ba0
Vladimir Panteleev <git@cy.md>
2021-09-05 17:36:44 +0000

    drunner: Improve error handling, add -of support

--- drunner.d
+++ drunner.d
@@ -22,29 +22,44 @@ if exist "%~f1.exe" del "%~f1.exe"
 call dbuild "%~f1"
 if exist "%~f1.exe" start "%~f1" /B /WAIT "%~f1.exe"
 */
-	args = args[1..$]; // discard self
-
-	string[] buildOptions;
-	while (args.length && args[0].startsWith('-'))
+	try
 	{
-		buildOptions ~= args[0];
-		args = args[1..$];
+		args = args[1..$]; // discard self
+
+		string[] buildOptions;
+		string exe;
+		while (args.length && args[0].startsWith('-'))
+		{
+			if (args[0].startsWith("-of"))
+				exe = args[0][3..$];
+			buildOptions ~= args[0];
+			args = args[1..$];
+		}
+
+		enforce(args.length > 0, "No file");
+		auto source = setExtension(args[0], "d");
+		if (!exe)
+			exe = stripExtension(args[0]);
+		//if (exists(exe))
+		//	remove(exe);
+		//auto spawnProcess(string[] args) { std.stdio.stderr.writeln(args); return std.process.spawnProcess(args); }
+		auto result = spawnProcess([builder] ~ buildOptions ~ [source]).wait();
+		if (result)
+			return result;
+		enforce(exe.exists, "Executable was not created: " ~ exe);
+		enforce(getSize(exe) > 0, "Executable has zero size: " ~ exe);
+		enforce((cast(ubyte[])read(exe))[0]!=0, "Executable is corrupted: " ~ exe);
+		version (Windows)
+			return spawnProcess([exe.absolutePath] ~ args[1..$]).wait();
+		else
+		{
+			execv(exe, [exe] ~ args[1..$]);
+			assert(false);
+		}
 	}
-
-	enforce(args.length > 0, "No file");
-	auto source = setExtension(args[0], "d");
-	auto exe = stripExtension(args[0]);
-	//if (exists(exe))
-	//	remove(exe);
-	//auto spawnProcess(string[] args) { std.stdio.stderr.writeln(args); return std.process.spawnProcess(args); }
-	auto result = spawnProcess([builder] ~ buildOptions ~ [source]).wait();
-	if (result || !exists(exe) || getSize(exe)==0 || (cast(ubyte[])read(exe))[0]==0)
-		return 1;
-	version (Windows)
-		return spawnProcess([exe.absolutePath] ~ args[1..$]).wait();
-	else
+	catch (Exception e)
 	{
-		execv(exe, [exe] ~ args[1..$]);
-		assert(false);
+		stderr.writeln("drun: ", e.msg);
+		return 1;
 	}
 }
