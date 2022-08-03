f154abc5372d53b5f90a69fc30005b5c7fcfefa9
Vladimir Panteleev <git@cy.md>
2022-08-03 15:31:55 +0000

    applayout: Use createLinkTargets

--- applayout.d
+++ applayout.d
@@ -15,13 +15,13 @@ import std.stdio;
 import std.string;
 import std.typecons;
 
-import ae.sys.file : isReadableFile, linkTarget, ensureFileExists;
+import ae.sys.file : createLinkTargets;
 import ae.sys.persistence.core;
 
 void main()
 {
 	auto fn = "~/.config/applayout.txt".expandTilde;
-	if (!fn.isReadableFile) fn.linkTarget.ensureFileExists();
+	createLinkTargets(fn, true);
 	auto rules = FileCache!((string fn) => fn
 		.readText
 		.splitLines
