a8f6942b7bb714c225d8eb41a38de08f2262a048
Vladimir Panteleev <git@cy.md>
2023-05-17 08:06:42 +0000

    move: Add some Linux-only options

--- move.d
+++ move.d
@@ -9,10 +9,49 @@
 import ae.utils.funopt;
 import ae.utils.main;
 
-void move(string source, string target)
+void move(
+	Switch!("Atomically exchange SOURCE and TARGET", 'e') exchange,
+	Switch!("Do not overwrite an existing TARGET", 'n') noReplace,
+	Switch!("Create a whiteout object", 'w') whiteout,
+	string source,
+	string target,
+)
 {
-	static import std.file;
-	std.file.rename(source, target);
+	version (linux)
+	{
+		import core.sys.posix.fcntl : AT_FDCWD;
+		import core.sys.linux.fs : RENAME_EXCHANGE, RENAME_NOREPLACE, RENAME_WHITEOUT;
+		import std.exception : errnoEnforce;
+		import std.string : toStringz;
+		import ae.utils.math : eq;
+
+		uint flags;
+		if (exchange)
+			flags |= RENAME_EXCHANGE;
+		if (noReplace)
+			flags |= RENAME_NOREPLACE;
+		if (whiteout)
+			flags |= RENAME_WHITEOUT;
+
+		renameat2(
+			AT_FDCWD, source.toStringz, 
+			AT_FDCWD, target.toStringz,
+			flags
+		).eq(0).errnoEnforce("renameat2");
+	}
+	else
+	{
+		enforce(!exchange && !noReplace && !whiteout, "This option is only supported on Linux.");
+
+		static import std.file;
+		std.file.rename(source, target);
+	}
 }
+
+version (linux)
+{
+	extern(C) int renameat2(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, uint flags);
+}
+
 
 mixin main!(funopt!move);
