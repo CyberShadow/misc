fa7acaa37f6c628fa68d3d8035de95bc44c63648
Vladimir Panteleev <git@cy.md>
2023-08-25 15:37:55 +0000

    printenv: Initial commit

---
+++ printenv.d
@@ -0,0 +1,23 @@
+/**
+   Debug tool; prints environment and arguments.
+*/
+
+module printenv;
+
+import std.ascii;
+import std.file;
+import std.getopt;
+import std.process;
+import std.stdio;
+import std.string;
+
+void main(string[] args)
+{
+	stderr.writeln("Arguments:");
+	foreach (arg; args)
+		stderr.writeln(arg);
+
+	stderr.writeln("Environment:");
+	foreach (name, value; environment.toAA)
+		stderr.writefln("%s=%s", name, value);
+}
