b4f8ca833d85fa5137a837819ea785afd8d97acf
Vladimir Panteleev <git@cy.md>
2023-11-24 14:29:29 +0000

    ncurses_showkey: Initial commit

---
+++ ncurses_showkey.d
@@ -0,0 +1,30 @@
+#!/usr/bin/env dub
+/+ dub.sdl:
+ dependency "ae" version="==0.0.3236"
+ dependency "ncurses" version="==1.0.0"
++/
+
+/**
+   Show the ncurses codes and names of pressed keys.
+*/
+
+import std.stdio : writefln;
+import deimos.ncurses;
+
+void main()
+{
+    int ch;
+
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, true);
+
+	while (true)
+	{
+		ch = getch();
+		mvprintw(0, 0, "The key pressed is: %d / %s          ", ch, keyname(ch));
+	}
+
+    // endwin();
+}
